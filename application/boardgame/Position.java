361e7a24b5ef4472f7ac75d2e2b4651b6f529c3d
gmendes25 <gerson-hp1@hotmail.com>
2022-09-08 10:06:06 -0300

    Position

---
+++ application/boardgame/Position.java
@@ -0,0 +1,48 @@
+package application.boardgame;
+
+public class Position{
+    
+    //#region Attributes
+    private Integer row;
+
+    private Integer column;
+    //#endregion
+
+    //#region Constructor
+    /**
+     * Indica a posição. Linha e 
+     * @param row Linha
+     * @param column Coluna
+     */
+    public Position(Integer row, Integer column) {
+    this.column = column;
+    this.row = row;
+    }
+    //#endregion
+    
+    //#region Getters and Setters
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+    //#endregion
+
+    //#region toString
+    @Override
+    public String toString() {
+        return "Position =" + row + " ," + column;
+    }
+    //#endregion
+    
+}
