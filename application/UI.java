e917ec1239e2648bca550a7dd72eda6bd7fc9e4e
gmendes25 <gerson-hp1@hotmail.com>
2022-09-20 09:54:09 -0300

    Print possible moves

--- application/UI.java
+++ application/UI.java
@@ -10,6 +10,7 @@ import application.chess.Color;
 //Interface de usuario
 public class UI {
 
+    //#region CORES
     //PIECE COLOR
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
@@ -30,13 +31,18 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    //#endregion
 
+    //#region Limpador de terminal
     // https://stackoverflow.com/questions/2979383/java-clear-the-console
-
+    /**
+     * Limpar o terminal
+     */
     public static void clearScreen(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+    //#endregion
 
     public static ChessPosition readChessPosition(Scanner sc){
         try {
@@ -50,20 +56,47 @@ public class UI {
         }
     }
 
+    //#region Imprimir tabuleiro
+    /**
+     * Para imprimir na tela o tabuleiro sem os movimentos possiveis
+     * @param pieces peças que serão impressas
+     */
     public static void printBoard(ChessPiece[][] pieces){
         for(int i = 0; i < pieces.length;i++){
             System.out.print((8-i) + " ");
             for(int j=0; j<pieces.length;j++){
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
                 }
                 System.out.println();
             }
             System.out.println("  a b c d e f g h");
     }
 
-    public static void printPiece(ChessPiece piece){
+    /**
+     * Para imprimir na tela o tabuleiro com os movimentos possiveis com fundo colorido
+     * @param pieces peças que serão impressas
+     */
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
+        for(int i = 0; i < pieces.length;i++){
+            System.out.print((8-i) + " ");
+            for(int j=0; j<pieces.length;j++){
+                printPiece(pieces[i][j], possibleMoves[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println("  a b c d e f g h");
+    }
+
+    /**
+     * Para imprimir as peças no tabuleiro
+     * @param piece Peças que serão impressas
+     */
+    public static void printPiece(ChessPiece piece, boolean background){
+        if(background){
+            System.out.print(ANSI_BLUE_BACKGROUND);
+        }
         if(piece == null){
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
             } else{
                 if(piece.getColor() == Color.WHITE){
                 System.out.print(ANSI_WHITE+ piece + ANSI_RESET);
@@ -73,4 +106,5 @@ public class UI {
             }
             System.out.print(" ");
     }
+    //#endregion
 }
